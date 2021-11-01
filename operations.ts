e226975cc34625bfeb3b53fa24f0292f95fa0977
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-01 16:33:00 -0500

    Creado el archivo con la logica de las operaciones crud, con todas las operacioens crud

--- operations.ts
+++ operations.ts
@@ -8,3 +8,29 @@ exports.insertDocument = (db, document, nameCollection, callback) => {
     callback(result);
   });
 };
+
+exports.findsDocuments = (db, nameCollection, callback) => {
+  const collection = db.collection(nameCollection);
+  collection.find({}).toArray((err, docs) => {
+    assert.equal(err, null);
+    callback(docs);
+  });
+};
+
+exports.removeDocument = (db, document, nameCollection, callback) => {
+  const collection = db.collection(nameCollection);
+  collection.deleteOne(document, (err, result) => {
+    assert.equal(err, null);
+    console.log("Removed the document " + document);
+    callback(result);
+  });
+};
+
+exports.updateDocument = (db, document, update, nameCollection, callback) => {
+  const collection = db.collection(nameCollection);
+  collection.updateOne(document, { $set: update }, null, (err, result) => {
+    assert.equal(err, null);
+    console.log("The document was updated");
+    callback(result);
+  });
+};
