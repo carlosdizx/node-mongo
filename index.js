7723c365f5614ad06a393d38e0e6090096fe08d5
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-01 16:52:15 -0500

    implementadas las operfacioens en el index (solo js)

---
+++ index.js
@@ -0,0 +1,59 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+const dboperations = require("./operations");
+
+const url = "mongodb://localhost:27017/";
+const dbname = "conFusion";
+
+MongoClient.connect(url, (err, client) => {
+  assert.equal(err, null);
+  console.log("Connected correctly to server");
+
+  const db = client.db(dbname);
+  dboperations.insertDocument(
+    db,
+    { name: "Vadonut", description: "Test" },
+    "dishes",
+    (result) => {
+      console.log("Inserted document:\n" + result);
+
+      dboperations.findsDocuments(db, "dishes", (docs) => {
+        console.log("Found documents:\n" + docs);
+
+        dboperations.updateDocument(
+          db,
+          { name: "Vadonut" },
+          { description: "Updated test" },
+          "dishes",
+          (result) => {
+            console.log("The document was successfully updated " + result);
+
+            dboperations.findsDocuments(db, "dishes", (docs) => {
+              console.log("Found documents:\n" + docs);
+
+              db.dropCollection("dishes", (err, result) => {
+                assert.equal(err, null);
+                console.log(
+                  result
+                    ? "The collection was removed successfully"
+                    : "The collection is not removed"
+                );
+                client.close();
+              });
+            });
+            /**
+            dboperations.removeDocument(
+              db,
+              { name: "Vadonut" },
+              "dishes",
+              (result) => {
+                console.log("The document was removed " + result);
+              }
+            );
+            */
+          }
+        );
+      });
+    }
+  );
+});
